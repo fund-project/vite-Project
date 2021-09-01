f745ef8d3a3ea8884907c2e326889202af919a5a
liuhonglin <499571879@qq.com>
2021-09-01 14:23:11 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: liuhl
+ * @Date: 2021-09-01 09:37:30
+ * @LastEditors: liuhl
+ * @LastEditTime: 2021-09-01 14:00:09
+ * @Description: file content
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
+
 console.log(import.meta.env)
+
 createApp(App).mount('#app')
